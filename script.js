415dda4ac7b053b3035bad2a62a04a76135b8944
jamila-samadova <semedovac@gmail.com>
2024-12-01 19:32:20 +0400

    Ilk commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+// Burda randomEl adli bir funkiya yaratmisiq ve random 0 ilə 1 arasında təsadüfi onluq ədəd yaradır. 
+// Elementlərin sayına vurularaq təsadüfi indeks alınır. 
+// Math.floor() – Onluq hissəni goturur tam ədədə çevirir. 
+
+
+function randomEl(arr) {                                  
+    const rIndex = Math.floor(Math.random() * arr.length);   
+    return arr[rIndex];    
+}                                                           
+                                                               
+  const words = ["aysen", "anar", "aynur"];
+  const guessWord = randomEl(words).split("");          // Burda təsadüfi sözu seçir və hərflərə ayırir
+  const hiddenWord = guessWord.map(() => "_"); // daha sonra hərfləri "_" ilə əvəz edirik.
+  
+
+
+
+
+//   loop vasitəsilə gizli sözün hərflərini tapiriq. 
+  while (hiddenWord.includes("_")) {
+    alert(`Sizin gizli sozunuz: ${hiddenWord.join(" ")}`);     
+  
+    const letter = prompt("Bir herf daxil edin:").toLowerCase();
+  
+    if (guessWord.includes(letter)) {
+                                                // Tapılan hərfin bütün indekslərini doldururuq.
+      guessWord.forEach((item, index) => {
+        if (item === letter) {
+          hiddenWord[index] = letter;
+        }
+      });
+      alert(`Tebrikler! "${letter}" herfini tapdiniz.`);
+    } else {
+      alert(`Bele, "${letter}" sozde yoxdur.`);
+    }
+  }
+  
+  alert(`Tebrikler! Sozu tapdiniz: ${guessWord.join("")}`);
+  
